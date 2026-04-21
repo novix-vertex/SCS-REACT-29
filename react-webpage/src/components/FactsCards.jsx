08c03c1abfb8daa6a02d3150b2a7952b076db659
chirag jain <jchirag0610@gmail.com>
2026-04-22 01:48:48 +0530

    -factscards 2nd card done -1st and 3rd left

--- react-webpage/src/components/FactsCards.jsx
+++ react-webpage/src/components/FactsCards.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const FactsCards = () => {
   return (
-    <div>FactsCards</div>
+    <div className='factscards-section'>
+      <div className="card1 card"></div>
+      <div className="card2 card">
+        <button>Private & Group Lessons</button>
+      </div>
+      <div className="card3 card"></div>
+    </div>
   )
 }
 

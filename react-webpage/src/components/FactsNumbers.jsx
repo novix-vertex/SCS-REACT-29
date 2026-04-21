12ec7a02551c8977dfeab1c25470cce2058de4a6
chirag jain <jchirag0610@gmail.com>
2026-04-22 00:44:28 +0530

    facts numbers section done

--- react-webpage/src/components/FactsNumbers.jsx
+++ react-webpage/src/components/FactsNumbers.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const FactsNumbers = () => {
   return (
-    <div>FactsNumbers</div>
+    <div className='facts-num-section'>
+      <div className="hours-play fact">
+        <h2>12000+</h2>
+        <h4>Hours of play annually</h4>
+      </div>
+      <div className="retention-rate fact">
+        <h2>89%</h2>
+        <h4>Player Retention Rate</h4>
+      </div>
+      <div className="active-member fact">        <h2>1200+</h2>
+        <h4>Active Members</h4>
+      </div>
+      <div className="annual-tournaments fact">       <h2>125+</h2>
+        <h4>Annual Tournaments</h4>
+      </div>
+    </div>
   )
 }
 

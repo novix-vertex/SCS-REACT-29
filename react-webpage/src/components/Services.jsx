bb552d819ace79780ffc0376511aa59d89ee571d
chirag jain <jchirag0610@gmail.com>
2026-04-22 12:19:41 +0530

    services section card 1 and 2 done 3 left

--- react-webpage/src/components/Services.jsx
+++ react-webpage/src/components/Services.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='services-section'>
+      <div className="card1 card">
+        <h4 class="services">Services</h4>
+        <h3>At Horizon, we don't just play tennis - we live it. Since 2021, <br />our club has been a home for players of all levels, from eager <br />beginners to seasoned pros.</h3>
+
+        <h4 className='explore'>Explore More <i class="ri-arrow-right-up-long-line"></i></h4>
+
+      </div>
+      <div className="card2 card">
+        <button>Training Programs</button>
+        <div className="bottom">
+          <h4>Programs designed for all ages and abilities.</h4>
+          <i class="ri-arrow-right-up-long-line"></i>
+        </div>
+
+      </div>
+      <div className="card3 card"></div>
+
+    </div>
   )
 }
 

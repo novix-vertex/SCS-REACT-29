c62a6e0e81e38bc43572e9b73d2a00992cc22063
chirag jain <jchirag0610@gmail.com>
2026-04-22 14:56:45 +0530

    factscard card 3 done

--- react-webpage/src/components/FactsCards.jsx
+++ react-webpage/src/components/FactsCards.jsx
@@ -14,7 +14,30 @@ const FactsCards = () => {
         <button>Private & Group Lessons</button>
       </div>
 
-      <div className="card3 card"></div>
+      <div className="card3 card">
+        <div className="top">
+          <h3>100+</h3>
+          <h4>Pro Coaches</h4>
+          <h5>Certified professional ready to boost your game from first serve to tournament level</h5>
+        </div>
+        <div className="bottom">
+          <div className="expert-level level">
+            <h4 className="level-text">Beginner</h4>
+            <h4 className="dots"><span><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i></span></h4>
+            <h4 className='count'>55</h4>
+          </div>
+          <div className="expert-level level">
+            <h4 className="level-text">Intermediate</h4>
+            <h4 className="dots"><span><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i></span></h4>
+            <h4 className='count'>40</h4>
+          </div>
+          <div className="expert-level level">
+            <h4 className="level-text">Advanced</h4>
+            <h4 className="dots"><span><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i><i class="ri-circle-fill"></i></span></h4>
+            <h4 className='count'>35</h4>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

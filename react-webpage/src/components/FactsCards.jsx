00d0d698f2a8833e3a2288140e18123a5aa02d4e
chirag jain <jchirag0610@gmail.com>
2026-04-22 13:27:22 +0530

    facts card - card 1 done

--- react-webpage/src/components/FactsCards.jsx
+++ react-webpage/src/components/FactsCards.jsx
@@ -3,10 +3,17 @@ import React from 'react'
 const FactsCards = () => {
   return (
     <div className='factscards-section'>
-      <div className="card1 card"></div>
+
+      <div className="card1 card">
+        <h4 className='top'><i class="ri-home-8-line"></i></h4>
+        <h3>Professional hard courts <span>with tournament-grade lighting & climate control - play in</span> perfect conditions, in any season.</h3>
+        <h4 className='bottom'><span><span class="circle"></span></span>Game Mode</h4>
+      </div>
+
       <div className="card2 card">
         <button>Private & Group Lessons</button>
       </div>
+
       <div className="card3 card"></div>
     </div>
   )
